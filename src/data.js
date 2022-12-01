ccefc00d0b156bd48278544051f261273eaa2bbf
Suthar98 <sutharvictor10@gmail.com>
2022-12-01 18:56:48 +0530

    First commit

---
+++ src/data.js
@@ -0,0 +1,55 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      ratting: 4.0,
+      numReviews: 10,
+      description: 'High Quality Product',
+    },
+
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      ratting: 4.0,
+      numReviews: 10,
+      description: 'High Quality Product',
+    },
+
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      ratting: 4.0,
+      numReviews: 10,
+      description: 'High Quality Product',
+    },
+
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      ratting: 4.0,
+      numReviews: 10,
+      description: 'High Quality Product',
+    },
+  ],
+};
